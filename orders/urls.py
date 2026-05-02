ddc4ec54c39881a434afe258e923697d97e950e7
suprice4 <princecapendit@gmail.com>
2026-05-02 23:23:13 +0800

    Cleaned up

--- orders/urls.py
+++ orders/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.order_list, name='order_list'),
     path('<int:pk>/', views.order_detail, name='order_detail'),
+    path('AddNewOrder/', views.add_new_order, name='add-new-order'),
+    path('AddNewPlatform/', views.add_new_platform, name='add-new-platform'),
+    path('EditProfile/', views.edit_profile, name='edit-profile'),
 ]
--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .models import Order, Platform
+from .forms import OrderForm, PlatformForm
 
-# Create your views here.
+@login_required
+def order_list(request):
+    orders = Order.objects.all()
+    return render(request, 'orders/order_list.html', {'orders': orders})
+
+@login_required
+def order_detail(request, pk):
+    order = get_object_or_404(Order, pk=pk)
+    return render(request, 'orders/order_detail.html', {'order': order})
+
+@login_required
+def add_new_order(request):
+    if request.method == 'POST':
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('order_list')
+    else:
+        form = OrderForm()
+    return render(request, 'orders/addNewOrder.html', {'form': form})
+
+@login_required
+def add_new_platform(request):
+    if request.method == 'POST':
+        form = PlatformForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('order_list')
+    else:
+        form = PlatformForm()
+    return render(request, 'orders/addNewPlatform.html', {'form': form})
+
+@login_required
+def edit_profile(request):
+    if request.method == 'POST':
+        user = request.user
+        user.first_name = request.POST.get('first_name', '')
+        user.last_name = request.POST.get('last_name', '')
+        user.email = request.POST.get('email', '')
+        user.save()
+        return redirect('order_list')
+    return render(request, 'orders/editProfile.html', {'user': request.user})
